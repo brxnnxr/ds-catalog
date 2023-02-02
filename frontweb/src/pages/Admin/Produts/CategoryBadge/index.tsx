0e4bea18a6ca565caeaeceb7168b5dbc204c6e91
Brenner Campos <brennercampos96@hotmail.com>
2023-02-02 17:16:00 -0300

    ProductCrudForm html

--- frontweb/src/pages/Admin/Produts/CategoryBadge/index.tsx
+++ frontweb/src/pages/Admin/Produts/CategoryBadge/index.tsx
@@ -1,4 +1,5 @@
 import './styles.css';
+
 type Props = {
   name?: string;
 };
--- frontweb/src/pages/Admin/Produts/Form/index.tsx
+++ frontweb/src/pages/Admin/Produts/Form/index.tsx
@@ -1,5 +1,31 @@
+import './styles.css';
+
 const Form = () => {
-  return <h1>Tela Form</h1>;
+  return (
+    <div className="product-crud-container">
+      <div className="base-card product-crud-form-card">
+        <h1 className="product-crud-form-title">DADOS DO PRODUTO</h1>
+        <form action="">
+          <div className="row">
+            <div className="col-lg-6">
+              <input type="text" className="base-input form-control" />
+              <input type="text" className="base-input form-control" />
+              <input type="text" className="base-input form-control" />
+            </div>
+            <div className="col-lg-6">
+              <textarea name="" rows={10} className="base-input form-control">
+                {' '}
+              </textarea>
+            </div>
+          </div>
+          <div className="">
+            <button className="btn-outline-danger">CANCELAR</button>
+            <button className="btn-outline-primary">SALVAR</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Form;
